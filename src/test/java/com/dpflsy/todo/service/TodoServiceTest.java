36a79f0f8c4268c7de867c02a864c200cca14fd7
dpflsy <dpflsy@gmail.com>
2023-01-18 02:44:44 +0900

    쓰지 않는 import 삭제

--- src/test/java/com/dpflsy/todo/service/TodoServiceTest.java
+++ src/test/java/com/dpflsy/todo/service/TodoServiceTest.java
@@ -1,28 +1,14 @@
 package com.dpflsy.todo.service;
 
-import com.dpflsy.todo.controller.TodoController;
 import com.dpflsy.todo.domain.Todo;
 import com.dpflsy.todo.mapper.TodoMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
